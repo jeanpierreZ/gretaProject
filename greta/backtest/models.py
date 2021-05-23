6f105c70b5a6bad38e1a5e912cb3c88a73ab187c
jeanpierreZ <jeanpierrezingraff@gmail.com>
2021-05-23 18:37:19 +0200

    Create a Chart class to store charts and display user's charts.

--- greta/backtest/models.py
+++ greta/backtest/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Chart(models.Model):
+    image = models.ImageField(upload_to='media/')
+    objects = models.Manager()
+
+    def get_image(self):
+        return self.image
+
+    def __str__(self):
+        return self.image
+
+    # Metadata
+    class Meta:
+        ordering = ['image']
